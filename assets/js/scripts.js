7504f3fcb08af43f4c206006b0086cd28504a92a
Daniel Lozano <dn.lozano.m@gmail.com>
2016-04-15 01:00:22 +0200

    list cambiado a array almacenado en localstorage.
    Se ha modificado la lógica de la aplicación, en lugar de guardar objetos diferentes
    en localstorage se guarda un único objeto array que almacena el listado de
    elementos.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,31 +4,63 @@
 	var clearBtn = document.getElementById('clear');
 	var content = document.getElementById('content');
 	var containerRow = document.getElementById('container-row');
+	var list = [];
 
-	// TODO: primero rellenar contenido en función del contenido de localstorage
+	if (localStorage.getItem('list') != undefined &&
+		localStorage.getItem('list') != '') {
+		console.log(localStorage.getItem('list'));
+		list = JSON.parse(localStorage.getItem('list'));
+	};
+
 	// TODO: modificar elementos existentes en localstorage
 	// TODO: eliminar elementos del localstorage
 
 	// pintar cada elemento dentro de un contenedor
 	var renderItems = function(container) {
+		// obtener el array contenido.
+		if (list == undefined) {
+			return;
+		}
+		
 		// limpiar contenido y pintarlo de nuevo
 		container.innerHTML = '';
-		for (var i = 0; i < localStorage.length; i++) {
+		// TODO: este bucle no sirve para recorrer los elementos de un objeto pls... guardar un array dentro de un objeto actualizar dicho array.
+		for (var i = 0; i < list.length; i++) {
 			var element = document.createElement('p');
-			var storageItem = localStorage.getItem(localStorage.key(i));
 
 			// añadir estructura HTML a los elementos
 			element.className += 'storage-item';
-			element.innerHTML = "<span class='index'>" + i + "</span> <span class='body'>" + storageItem + "</span>";
+			element.innerHTML = "<span class='index'>" + i + "</span> <span class='body'>" + list[i] + "</span><button class='delete-item'>X</button>";
 
 			container.appendChild(element);
 		};
+		bindDeleteButtons();
+	};
+
+	var bindDeleteButtons = function() {
+		var deleteButtons = document.getElementsByClassName('delete-item');
+		
+		for (var i = 0; i < deleteButtons.length; i++) {
+			deleteButtons[i].addEventListener('click', function(e) {
+				// obtener el padre
+				var parent = this.parentNode;
+				var childrens = parent.childNodes;
+				var index = list.indexOf(childrens[2].innerText);
+
+				list.splice(index, 1);
+				
+				console.log(list);
+				localStorage.setItem('list', JSON.stringify(list));
+				renderItems(containerRow);
+			});
+		}
 	};
 
 	// Añadir botón para limpiar
 	clearBtn.addEventListener('click', function(e) {
 		e.preventDefault();
-		localStorage.clear();
+		list = [];
+		localStorage.setItem('list', list);
 		containerRow.innerHTML = '';
 	});
 
@@ -40,11 +72,9 @@
 		var storageValue = document.getElementsByName('value');
 		var orderValue = order[0].value
 		
-		if (orderValue === '') {
-			// obtener el la última key
-			orderValue = localStorage.length;
-		}
-		localStorage.setItem(orderValue, storageValue[0].value);
+		list.push(storageValue[0].value);
+
+		localStorage.setItem('list', JSON.stringify(list));
 
 		renderItems(containerRow);
 
@@ -60,5 +90,7 @@
 		}
 	});
 
+	// Eliminar individualmente el contenido de los elementos
+	
 	renderItems(containerRow);
 }());
